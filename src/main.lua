4df16cb355b93548a29303cc69f568f7339a5f1a
Frank Uhlig <uhlig.frank@gmail.com>
2021-11-11 20:42:17 +0100

    chg: indicate bug velocity using tiny head

--- src/main.lua
+++ src/main.lua
@@ -209,6 +209,16 @@ function love.draw()
       love.graphics.setColor(1, 0, 0)
     end
     love.graphics.circle('fill', bug.pos.x, bug.pos.y, bug.shape.width)
+
+    -- for this visualization to work properly, we need to rework the world
+    -- movement and have the bug at rest when its vel is zero
+    local norm = math.sqrt(bug.vel.x * bug.vel.x + bug.vel.y * bug.vel.y)
+    local factor = bug.shape.width * 0.9 / norm
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle('fill',
+      bug.pos.x + bug.vel.x * factor,
+      bug.pos.y + bug.vel.y * factor, bug.shape.width / 5)
   end
 
   love.graphics.setBackgroundColor(0.1, 1, 0.3)
