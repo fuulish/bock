109038138f078972b332c659aad0ddc6b9f76093
Frank Uhlig <uhlig.frank@gmail.com>
2021-11-29 18:54:46 +0100

    chg: score output centered

--- src/main.lua
+++ src/main.lua
@@ -754,7 +754,7 @@ function love.draw()
   if game_state == State.fin then
     love.graphics.setBackgroundColor(0, 0, 1)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("SCORE: " .. score, width / 2, height / 2)
+    love.graphics.print("SCORE: " .. score, width / 2 - 40, height / 2)
     return
   end
 
